98c02a2faea5debc946dd16d2f5f18bc012d7661
mxmlnkn <mxmlnkn@github.de>
2018-04-08 16:29:19 +0200

    add trailing zeros

--- uncertainValueToStr.py
+++ uncertainValueToStr.py
@@ -15,10 +15,17 @@ def uncertainValueToStr( x, sx ):
 
     # Format exponent and error https://www.ncbi.nlm.nih.gov/pmc/articles/PMC4483789/
     # Can't find that particular standard right now, but I thought some standard specified two significant digits for the first digit being < 3 and else one significant digits on the errors. And of course the mean should have as much precision as the error has
-    nDigits = 2 if getFirstDigit( sx ) in [1,2] else 1
+    nDigitsErr = 2 if getFirstDigit( sx ) in [1,2] else 1
     magSx   = getMagnitude( sx )
-    sxShort = str( roundToSignificant( sx, nDigits ) )
+    sxShort = str( roundToSignificant( sx, nDigitsErr ) )
     # pad with 0s if necessary, showing the rounding, i.e. avoid situations like 2.093 +- 0.02 (0.0213...)
-    sxShort += '0' * ( nDigits - ( len(sxShort) - min( sxShort.find(d) for d in '123456789' if d in sxShort ) ) )
-    xShort  = np.around( x / 10**magSx, nDigits-1 ) * 10**magSx
-    return str( xShort ), sxShort
+    def existingDigits( s ):
+        s = str(s)
+        firstNonZero = [ s.find(d) for d in '123456789' if d in s ]
+        firstNonZero = min( firstNonZero ) if len( firstNonZero ) > 0 else len(s)
+        nDigits = np.sum( [ s[ firstNonZero: ].split('e')[0].count(d) for d in '0123456789' ] )
+        #print( s, "first non-zero:", firstNonZero, "# digits:", nDigits )
+        return nDigits
+    xShort  = str( np.around( x / 10**magSx, nDigitsErr ) * 10**magSx )
+    xShort += '0' * max( 0, nDigitsErr - existingDigits( str( np.around( x / 10**magSx, nDigitsErr ) ).split('.')[-1] ) )
+    return xShort, sxShort
